b8bb94a72ae83fff5b901ef2dc4c0217bbb2241c
syahraqagr <32156061+syahraqagr@users.noreply.github.com>
2018-06-10 19:55:08 +0700

    Update groupbot.py

--- examples/groupbot.py
+++ examples/groupbot.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from linepy import *
 
-line = LINE('EMAIL', 'PASSWORD')
-#line = LINE('AUTHTOKEN')
+#cl = LINE('EMAIL', 'PASSWORD')
+cl = LINE('Et1fd26gCtgbLdd2jDgb.ggNCLqZ5irfKOvdzgQfq2W.EcY3RGwnjaFPDOgsRiTBxCjdpA9By/UR4Yq00WtS09M=')
 
-line.log("Auth Token : " + str(line.authToken))
-line.log("Timeline Token : " + str(line.tl.channelAccessToken))
+cl.log("Auth Token : " + str(cl.authToken))
+cl.log("Timeline Token : " + str(cl.tl.channelAccessToken))
 
 # Initialize OEPoll with LINE instance
-oepoll = OEPoll(line)
+oepoll = OEPoll(cl)
 
 # Receive messages from OEPoll
 def RECEIVE_MESSAGE(op):
@@ -32,27 +32,27 @@ def RECEIVE_MESSAGE(op):
             # Check only group chat
             if msg.toType == 2:
                 # Chat checked request
-                line.sendChatChecked(receiver, msg_id)
+                cl.sendChatChecked(receiver, msg_id)
                 # Get sender contact
-                contact = line.getContact(sender)
+                contact = cl.getContact(sender)
                 # Command list
                 if text.lower() == 'hi':
-                    line.log('[%s] %s' % (contact.displayName, text))
-                    line.sendMessage(receiver, 'Hi too! How are you?')
+                    cl.log('[%s] %s' % (contact.displayName, text))
+                    cl.sendMessage(receiver, 'Hi too! How are you?')
                 elif text.lower() == '/author':
-                    line.log('[%s] %s' % (contact.displayName, text))
-                    line.sendMessage(receiver, 'My author is linepy')
+                    cl.log('[%s] %s' % (contact.displayName, text))
+                    cl.sendMessage(receiver, 'My author is linepy')
     except Exception as e:
-        line.log("[RECEIVE_MESSAGE] ERROR : " + str(e))
+        cl.log("[RECEIVE_MESSAGE] ERROR : " + str(e))
     
 # Auto join if BOT invited to group
 def NOTIFIED_INVITE_INTO_GROUP(op):
     try:
         group_id=op.param1
         # Accept group invitation
-        line.acceptGroupInvitation(group_id)
+        cl.acceptGroupInvitation(group_id)
     except Exception as e:
-        line.log("[NOTIFIED_INVITE_INTO_GROUP] ERROR : " + str(e))
+        cl.log("[NOTIFIED_INVITE_INTO_GROUP] ERROR : " + str(e))
 
 # Add function to OEPoll
 oepoll.addOpInterruptWithDict({
